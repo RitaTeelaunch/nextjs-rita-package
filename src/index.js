2b4a2204177d0335ae7263b63256a05c5353d8e8
RitaTeelaunch <rita@teelaunch.com>
2024-08-21 11:39:02 +0300

    button change color

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import useCountry from "./hooks/useCountry.js";
-export {useCountry}
+// import useCountry from "./hooks/useCountry.js";
+// export {useCountry}
+
+ const Animated=()=>{
+    return(
+        <>
+            <p>Hello Rita</p>
+            <button type="primary">change</button>
+        </>
+    )
+}
+export default Animated
